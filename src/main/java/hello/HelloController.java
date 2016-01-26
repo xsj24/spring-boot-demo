1e07dec579d74d406e25c6e08c028cb1688be475
xsj <1187080550@qq.com>
2016-01-26 17:10:22 +0800

    初始化

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("hello")
+	@ResponseBody
+	public String hello() {
+		return "Hello World!";
+	}
+	
+	// 本地的静态资源都配置在了classpath下
+	@RequestMapping(value="hello", method=RequestMethod.GET, params="foo")
+	public String hello(String foo) {
+		System.out.println(foo);
+		return "test.html";
+	}
+}
